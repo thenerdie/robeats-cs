d06cdae0110708f0c26eb3fc96d6bf06b66a6155
ThErEgEnErAtOr <stirmanjr@gmail.com>
2020-06-05 23:06:35 -0400

    added stupid project file lol

--- src/StarterGui/Client.client.lua
+++ src/StarterGui/Client.client.lua
@@ -25,6 +25,8 @@ local Settings = require(Utils.Settings)
 local Graph = require(Frameworks.Graph)
 local UI = require(Frameworks.UI)
 
+print("bruh")
+
 for i, screen in pairs(PlayerGui:WaitForChild("Screens"):GetChildren()) do
 	ScreenUtil:NewScreen(screen.Name, require(screen))
 end
