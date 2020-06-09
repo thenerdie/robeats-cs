962d17b13a164ced3da2bd42faa013b889ac55e7
blizzo <stirmanjr@gmail.com>
2020-06-09 15:34:03 -0400

    Implement Options menu
    Just getting the framework set up for an options menu

--- src/StarterGui/Screens/OptionsScreen.lua
+++ src/StarterGui/Screens/OptionsScreen.lua
@@ -22,8 +22,111 @@ local self = {}
 local handle = {}
 local tree = {}
 
-local function NewOption(props)
-	return UI.new("Option",props)
+local optionNumber = 0
+local maxOptionNumber = 7
+
+local function Option(name, type, bound)
+	local boundFire = "Update"..bound
+	self[bound], self[boundFire] = Roact.createBinding(type == "number" and 0 or "")
+	optionNumber = optionNumber + 1
+	return Roact.createElement("Frame", {
+		Size = UDim2.new(1,0,0.15,0);
+		Position = UDim2.new(0, 0, (optionNumber-1) / maxOptionNumber, 0);
+		BackgroundTransparency = 1;
+	}, {
+		Name = Roact.createElement("TextLabel", {
+			BackgroundTransparency = 1;
+			Font = Enum.Font.GothamBlack;
+			TextColor3 = Color3.fromRGB(255, 255, 255);
+			Text = name;
+			TextSize = 30;
+			Position = UDim2.new(0,0,0,0);
+			Size = UDim2.new(0.2,0,1,0);
+		});
+		OptionValue = Roact.createElement("ImageLabel", {
+			AnchorPoint = Vector2.new(0, 0.5),
+			Size = UDim2.new(0.2,0,0.5,0),
+			Position = UDim2.new(0.2,0,0.5,0),
+			BorderSizePixel = 0,
+			BackgroundTransparency = 1,
+			ScaleType = Enum.ScaleType.Slice,
+			Image = "rbxassetid://2790382281",
+			SliceCenter = Rect.new(4, 4, 252, 252),
+			SliceScale = 1,
+			ImageColor3 = Color3.fromRGB(35, 35, 35)
+		},{
+			Data = Roact.createElement("TextLabel", {
+				AnchorPoint = Vector2.new(0.5, 0.5),
+				Size = UDim2.new(0.85, 0, 0.7, 0),
+				Position = UDim2.new(0.5, 0, 0.5, 0),
+				BorderSizePixel = 0,
+				BackgroundTransparency = 1,
+				Text = self[bound],
+				Font = Enum.Font.GothamBlack,
+				TextScaled = true,
+				TextWrapped = true,
+				TextColor3 = Color3.fromRGB(179, 179, 179)
+			})
+		});
+		Plus = Roact.createElement("ImageLabel", {
+			AnchorPoint = Vector2.new(0, 0.5),
+			Size = UDim2.new(0.2,0,0.5,0),
+			Position = UDim2.new(0.4,5,0.5,0),
+			BorderSizePixel = 0,
+			BackgroundTransparency = 1,
+			ScaleType = Enum.ScaleType.Slice,
+			Image = "rbxassetid://2790382281",
+			SliceCenter = Rect.new(4, 4, 252, 252),
+			SliceScale = 1,
+			ImageColor3 = Color3.fromRGB(32, 221, 32)
+		},{
+			Data = Roact.createElement("TextButton", {
+				AnchorPoint = Vector2.new(0.5, 0.5),
+				Size = UDim2.new(0.85, 0, 0.7, 0),
+				Position = UDim2.new(0.5, 0, 0.5, 0),
+				BorderSizePixel = 0,
+				BackgroundTransparency = 1,
+				Text = "+",
+				Font = Enum.Font.GothamBlack,
+				TextScaled = true,
+				TextWrapped = true,
+				TextColor3 = Color3.fromRGB(255, 255, 255),
+				[Roact.Event.MouseButton1Click] = function(rbx)
+					local optionValue = Settings:Increment(bound, 1)
+					self[boundFire](optionValue)
+				end
+			})
+		});
+		Minus = Roact.createElement("ImageLabel", {
+			AnchorPoint = Vector2.new(0, 0.5),
+			Size = UDim2.new(0.2,0,0.5,0),
+			Position = UDim2.new(0.6,5,0.5,0),
+			BorderSizePixel = 0,
+			BackgroundTransparency = 1,
+			ScaleType = Enum.ScaleType.Slice,
+			Image = "rbxassetid://2790382281",
+			SliceCenter = Rect.new(4, 4, 252, 252),
+			SliceScale = 1,
+			ImageColor3 = Color3.fromRGB(209, 47, 47)
+		},{
+			Data = Roact.createElement("TextButton", {
+				AnchorPoint = Vector2.new(0.5, 0.5),
+				Size = UDim2.new(0.85, 0, 0.7, 0),
+				Position = UDim2.new(0.5, 0, 0.5, 0),
+				BorderSizePixel = 0,
+				BackgroundTransparency = 1,
+				Text = "-",
+				Font = Enum.Font.GothamBlack,
+				TextScaled = true,
+				TextWrapped = true,
+				TextColor3 = Color3.fromRGB(255, 255, 255),
+				[Roact.Event.MouseButton1Click] = function(rbx)
+					local optionValue = Settings:Increment(bound, -1)
+					self[boundFire](optionValue)
+				end
+			})
+		});
+	})
 end
 
 local function Base()
@@ -52,11 +155,8 @@ local function Base()
 				Size = UDim2.new(0.975,0,0.95,0);
 				Position = UDim2.new(0.5,0,0.5,0);
 				BackgroundTransparency = 1;
-				
-				
-				--[[Children = {
-					O = Roact.createElement()
-				},]]
+			}, { -- OPTIONS GO HERE
+				ScrollSpeed = Option("Scroll Speed", "number", "ScrollSpeed")
 			}),
 			BackButton = Roact.createElement("ImageButton", {
 				BackgroundColor3 = Color3.fromRGB(232, 49, 49),
