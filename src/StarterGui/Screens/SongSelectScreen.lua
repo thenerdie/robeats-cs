1e4e6c30c428c3fa9f7485869fbd33619c9c0f90
ItzFireable <fireableplayz@gmail.com>
2020-06-09 12:27:31 +0300

    Removed unneeded print() (caused lag)

--- src/StarterGui/Screens/SongSelectScreen.lua
+++ src/StarterGui/Screens/SongSelectScreen.lua
@@ -42,7 +42,6 @@ local function getNumSlots()
 end
 
 local function SongButton(instance, song, songNum)
-	print(song)
 	return Roact.createElement("TextButton", {
 		Size=UDim2.new(1,0,0,45);
 		BackgroundColor3=song:GetButtonColor();
