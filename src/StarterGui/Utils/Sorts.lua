688bf21972efb1ccbc3ea43188be19e8e9329523
blizzo <stirmanjr@gmail.com>
2020-07-17 19:49:10 -0400

    Fix LB sort function

--- src/StarterGui/Utils/Sorts.lua
+++ src/StarterGui/Utils/Sorts.lua
@@ -1,6 +1,6 @@
 return {
     Leaderboard = function(a, b)
-        if a.rating == 0 and b.rating == 0 then
+        if a.rating == b.rating then
             return a.score > b.score
         end
         return a.rating > b.rating
