811135599252b790e1bccb9a9d285625d4cd7cdb
blizzo <stirmanjr@gmail.com>
2020-07-20 18:42:16 -0400

    Added a ton of properties for a multiplayer room

--- src/ServerScriptService/ServerScripts/ServerClasses/MultiplayerRoom.lua
+++ src/ServerScriptService/ServerScripts/ServerClasses/MultiplayerRoom.lua
@@ -11,9 +11,39 @@ function MultiplayerRoom:new()
     room.song = nil
     room.players = {}
     room.host = {}
+    room.isplaying = false
+    room.mods = {}
+
+    local function add(tab, item)
+        tab[#tab+1] = item
+    end
+
+    local function remove(tab, rmFn)
+        for i, v in pairs(tab) do
+            if rmFn(v) == true then
+                v = nil
+            end
+        end
+    end
 
     function room:AddPlayer(plr)
         room[#room+1] = plr
+    end
+
+    function room:RemovePlayer(plr)
+        remove(room.players, function(item)
+            return plr.UserId == item.UserId
+        end)
+    end
+
+    function room:ApplyMod(mod)
+        add(room.mods, mod)
+    end
+
+    function room:UnapplyMod(mod)
+        remove(room.mods, function(item)
+            return item.Name == mod.Name
+        end)
     end
 
     function room:ChangeSong(song)
@@ -21,7 +51,11 @@ function MultiplayerRoom:new()
     end
     
     function room:StartGame()
-        
+        room.isplaying = true
+    end
+
+    function room:StopGame()
+        room.isplaying = true
     end
 
     return room    
